1adda5f754f2f5943fc592f8738f59e9d8da8ccb
toshi <why@softbank.ne.jp>
2020-01-22 01:51:09 +0900

    asrasd

--- django_app/settings.py
+++ django_app/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 import dj_database_url
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/book/static')
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static'),
