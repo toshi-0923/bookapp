5db950fba0ba1b9d79f95144ea2004ab83bf51ab
toshi <why@softbank.ne.jp>
2020-01-22 16:41:21 +0900

    sya

--- django_app/wsgi.py
+++ django_app/wsgi.py
@@ -1,8 +1,7 @@
 import os
 
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
