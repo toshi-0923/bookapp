fe226600597a2822c904f473951f1a7fdca4b5dc
toshi <why@softbank.ne.jp>
2020-02-27 19:40:05 +0900

    aa

--- actress/views.py
+++ actress/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Actress
 from .forms import CreateForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url="/admin/login/")
 def index(request):
     createform = CreateForm()
     data = Actress.objects.all()
